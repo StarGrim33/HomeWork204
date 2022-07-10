cecab36b9a160c2286f76d05f6ad783f24e5c377
StarGrim33 <prosky33@gmail.com>
2022-07-10 12:55:09 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace JuniorHomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minNumber = 1;
+            int maxNumber = 27;
+            int minTripleNumber = 100;
+            int maxTripleNumber = 999;
+            int counterMultipleNumbers = 0;
+
+            Random random = new Random();
+            int numberN = random.Next(minNumber, maxNumber);
+
+            for (int currentNumber = 0; currentNumber < maxTripleNumber; currentNumber += numberN)
+            {
+                if (currentNumber >= minTripleNumber)
+                {
+                    counterMultipleNumbers++;
+                }
+            }
+
+            Console.WriteLine("Количество трехзначных натуральных чисел кратных " + numberN + " равно " + counterMultipleNumbers);
+            Console.ReadLine();
+        }
+    }
+}
